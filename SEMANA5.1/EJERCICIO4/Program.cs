92b83c42d5b6c333b7d81e29934559cd321c6dc4
Casenciom <ca.asenciom@uea.edu.ec>
2026-01-09 23:52:17 -0500

    Add files via upload

---
+++ SEMANA5.1/EJERCICIO4/Program.cs
@@ -0,0 +1,36 @@
+﻿// EJERCICIO 4 Escribir un programa que pregunte al usuario los 
+//números ganadores de la lotería primitiva, los almacene en una lista y los muestre por pantalla ordenados de menor a mayor.
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Lista para almacenar los números ganadores
+        List<int> numeros = new List<int>();
+
+        Console.WriteLine("Ingrese los números ganadores de la lotería primitiva:");
+
+        // Pedir los números al usuario (6 números)
+        for (int i = 1; i <= 6; i++)
+        {
+            Console.Write("Número " + i + ": ");
+            int numero = int.Parse(Console.ReadLine());
+            numeros.Add(numero);
+        }
+
+        // Ordenar la lista de menor a mayor
+        numeros.Sort();
+
+        Console.WriteLine("\nNúmeros ordenados de menor a mayor:");
+
+        // Mostrar los números ordenados
+        foreach (int num in numeros)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+
---
+++ SEMANA5.1/EJERCICIO5/Program.cs
@@ -0,0 +1,32 @@
+﻿// EJERICIO 5 Escribir un programa que almacene en una lista los números del 1 al 10 
+//y los muestre por pantalla en orden inverso separados por comas.
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Lista para almacenar los números del 1 al 10
+        List<int> numeros = new List<int>();
+
+        // Agregar los números del 1 al 10 a la lista
+        for (int i = 1; i <= 10; i++)
+        {
+            numeros.Add(i);
+        }
+
+        // Mostrar los números en orden inverso separados por comas
+        for (int i = numeros.Count - 1; i >= 0; i--)
+        {
+            Console.Write(numeros[i]);
+
+            if (i > 0)
+            {
+                Console.Write(", ");
+            }
+        }
+    }
+}
+
